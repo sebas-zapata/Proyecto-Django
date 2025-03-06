abc10b668cd99e100cb5a6174e61b5df65507d2c
Sebastian Zapata <zapatajuan351@gmail.com>
2025-03-06 11:21:02 -0500

    Modificacion de archivos del proyecto

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import registrar_cliente, lista_clientes, inicio
+from .views import registrar_cliente, eliminar_cliente, editar_cliente, lista_clientes, inicio
 
 urlpatterns = [
     path('', inicio, name='inicio'),
     path('clientes/', lista_clientes, name='lista_clientes'),
     path('registrar/', registrar_cliente, name='registrar_cliente'),
+    path('eliminar/<int:cliente_id>/', eliminar_cliente, name='eliminar_cliente'),
+    path('editar/<int:cliente_id>/', editar_cliente, name='editar_cliente'),
 ]
--- mi_app/views.py
+++ mi_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Cliente
 from .forms import ClienteForm
 
@@ -22,3 +22,23 @@ def registrar_cliente(request):
 def lista_clientes(request):
     clientes = Cliente.objects.all()  # Obtener todos los clientes
     return render(request, 'clientes.html', {'clientes': clientes})
+
+
+# Vista para editar Clientes
+def editar_cliente(request, cliente_id):
+    cliente = get_object_or_404(Cliente, id=cliente_id)  # Obtener el cliente por ID
+    if request.method == "POST":
+        form = ClienteForm(request.POST, instance=cliente)  # Cargar datos en el formulario
+        if form.is_valid():
+            form.save()  # Guardar cambios
+            return redirect('lista_clientes')  # Redirigir a la lista de clientes
+    else:
+        form = ClienteForm(instance=cliente)  # Cargar formulario con datos existentes
+    return render(request, 'editar_clientes.html', {'form': form, 'cliente': cliente})
+
+
+# Vista para eliminar clientes
+def eliminar_cliente(request, cliente_id):
+    cliente = get_object_or_404(Cliente, id=cliente_id)  # Buscar el cliente por ID
+    cliente.delete()  # Eliminar el cliente
+    return redirect('lista_clientes')  # Redirigir a la lista de clientes
